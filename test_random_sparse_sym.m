75ebf12113a90cc1a3cdb7a9bd07448f9f8ecc3e
Daniela di Serafino <daniela.diserafino@unicampania.it>
2018-09-30 23:25:47 +0200

    Cleanup of test programs.

--- test_random_sparse_sym.m
+++ test_random_sparse_sym.m
@@ -2,15 +2,15 @@
 % Run constraint-preconditioned Krylov solvers on random symmetric
 % regularized saddle-point systems.
 %
-% Daniela di Serafino, Nov. 10, 2017
+% Daniela di Serafino, Aug. 30, 2018
 
 % clear;
 % close all;
 
-n = 300; m = 100;
+n = 400; m = 150;
 
 % Build Q.
-densityQ = 0.02;
+densityQ = 0.01;
 rcQ = 1.e-8;
 Q = sprandsym(n,densityQ,rcQ,1);
 % Add alpha to nn diagonal entries of Q. Comment the following lines
@@ -28,7 +28,7 @@ Q = Q + spdiags(dq,0,n,n);
 
 
 % Build A.
-rcA = 1.e-1;
+rcA = 1.; % 1.e-3;  % 1.e-1;
 densityA = densityQ;
 A = sprand(m,n,densityA,rcA);
 % A = 1e8*A;
@@ -39,8 +39,9 @@ density = 0.8;
 beta = 1e-4;
 % Different ways of building C. Uncomments your choice.
 % C = spdiags(diag(sprandsym(m,0,rcC,1)),0,m,m);           % C diag spd
-% C = spdiags(abs(diag(sprand(m,m,density,rcC))),0,m,m);   % C diag with entries >= 0
-C = beta*speye(m);                                       % C = beta*I
+C = spdiags(abs(diag(sprand(m,m,density,rcC))),0,m,m);   % C diag with entries >= 0
+% C = beta*speye(m);                                       % C = beta*I
+% C = speye(m);
 
 K = [Q  A' ; A  -C];
 b = rand(n+m, 1);
@@ -71,19 +72,22 @@ pause
 
 cpk1 = @cpcg;
 cpk_string1 = 'CPCG';
-cpk22 = @cpcglanczos2;
-cpk_string22 = 'CPCGLANCZOS2';
 cpk2 = @cpcglanczos;
 cpk_string2 = 'CPCGLANCZOS';
 cpk3 = @cpdqgmres;
 cpk_string3 = 'CPDQGMRES';
-cpk4 = @cpgmres;
-cpk_string4 = 'CPGMRES';
+cpk4 = @cpminres;
+cpk_string4 = 'CPMINRES';
+cpk5 = @cpsymmlq;
+cpk_string5 = 'CPSYMMLQ';
+cpk6 = @cpsymmlq_v0;
+cpk_string6 = 'CPSYMMLQ-V0';
 
-opts.print = false;       % true/false;
+opts.print = true;       % true/false;
 opts.atol = 1.0e-6;
 opts.rtol = 1.0e-6;
-opts.itmax = 280;
+opts.btol = 0.0;
+opts.itmax = 500;
 opts.mem = 2;             % 2 for symmetric systems;
 opts.restart = 20;
 
@@ -100,21 +104,10 @@ fprintf('\n\n******************* %s *******************\n\n', cpk_string1)
 [cpk1x,  cpk1stats,  cpk1flag] =  reg_cpkrylov(cpk1, b, Q, A, C, G, opts);
 cpk1x1  = cpk1x(1:n);
 cpk1x2  = cpk1x(n+1:n+m);
-fprintf('\n%s - rel err in (x, y) (refsol bsl): %7.1e', cpk_string1, norm(x-cpk1x)/norm(x));
-fprintf('\n%s - rel err in x (refsol bsl): %7.1e', cpk_string1, norm(x(1:n)-cpk1x1)/norm(x(1:n)));
-fprintf('\n%s - rel err in y (refsol bsl): %7.1e\n', cpk_string1, norm(x(n+1:n+m)-cpk1x2)/norm(x(n+1:n+m)));
+fprintf('\n%s - rel err in (x, y) (refsol bsl): %8.2e', cpk_string1, norm(x-cpk1x)/norm(x));
+fprintf('\n%s - rel err in x (refsol bsl): %8.2e', cpk_string1, norm(x(1:n)-cpk1x1)/norm(x(1:n)));
+fprintf('\n%s - rel err in y (refsol bsl): %8.2e\n', cpk_string1, norm(x(n+1:n+m)-cpk1x2)/norm(x(n+1:n+m)));
 fprintf('\n%s - iters = %d\n', cpk_string1, cpk1stats.niters);
-pause
-
-% CPCGLANCZOS2.
-fprintf('\n\n******************* %s *******************\n\n', cpk_string22)
-[cpk22x,  cpk22stats,  cpk22flag] =  reg_cpkrylov(cpk22, b, Q, A, C, G, opts);
-cpk22x1  = cpk22x(1:n);
-cpk22x2  = cpk22x(n+1:n+m);
-fprintf('\n%s - rel err in (x, y) (refsol bsl): %7.1e', cpk_string22, norm(x-cpk22x)/norm(x));
-fprintf('\n%s - rel err in x (refsol bsl): %7.1e', cpk_string22, norm(x(1:n)-cpk22x1)/norm(x(1:n)));
-fprintf('\n%s - rel err in y (refsol bsl): %7.1e\n', cpk_string22, norm(x(n+1:n+m)-cpk22x2)/norm(x(n+1:n+m)));
-fprintf('\n%s - iters = %d\n', cpk_string22, cpk22stats.niters);
 pause
 
 % CPCGLANCZOS.
@@ -122,9 +115,9 @@ fprintf('\n\n******************* %s *******************\n\n', cpk_string2)
 [cpk2x,  cpk2stats,  cpk2flag] =  reg_cpkrylov(cpk2, b, Q, A, C, G, opts);
 cpk2x1  = cpk2x(1:n);
 cpk2x2  = cpk2x(n+1:n+m);
-fprintf('\n%s - rel err in (x, y) (refsol bsl): %7.1e', cpk_string2, norm(x-cpk2x)/norm(x));
-fprintf('\n%s - rel err in x (refsol bsl): %7.1e', cpk_string2, norm(x(1:n)-cpk2x1)/norm(x(1:n)));
-fprintf('\n%s - rel err in y (refsol bsl): %7.1e\n', cpk_string2, norm(x(n+1:n+m)-cpk2x2)/norm(x(n+1:n+m)));
+fprintf('\n%s - rel err in (x, y) (refsol bsl): %8.2e', cpk_string2, norm(x-cpk2x)/norm(x));
+fprintf('\n%s - rel err in x (refsol bsl): %8.2e', cpk_string2, norm(x(1:n)-cpk2x1)/norm(x(1:n)));
+fprintf('\n%s - rel err in y (refsol bsl): %8.2e\n', cpk_string2, norm(x(n+1:n+m)-cpk2x2)/norm(x(n+1:n+m)));
 fprintf('\n%s - iters = %d\n', cpk_string2, cpk2stats.niters);
 pause
 
@@ -133,33 +126,61 @@ fprintf('\n\n******************* %s - mem %d *******************\n\n', cpk_strin
 [cpk3x,  cpk3stats,  cpk3flag] =  reg_cpkrylov(cpk3, b, Q, A, C, G, opts);
 cpk3x1  = cpk3x(1:n);
 cpk3x2  = cpk3x(n+1:n+m);
-fprintf('\n%s - rel err in (x, y) (refsol bsl): %7.1e', cpk_string3, norm(x-cpk3x)/norm(x));
-fprintf('\n%s - rel err in x (refsol bsl): %7.1e', cpk_string3, norm(x(1:n)-cpk3x1)/norm(x(1:n)));
-fprintf('\n%s - rel err in y (refsol bsl): %7.1e\n', cpk_string3, norm(x(n+1:n+m)-cpk3x2)/norm(x(n+1:n+m)));
+fprintf('\n%s - rel err in (x, y) (refsol bsl): %8.2e', cpk_string3, norm(x-cpk3x)/norm(x));
+fprintf('\n%s - rel err in x (refsol bsl): %8.2e', cpk_string3, norm(x(1:n)-cpk3x1)/norm(x(1:n)));
+fprintf('\n%s - rel err in y (refsol bsl): %8.2e\n', cpk_string3, norm(x(n+1:n+m)-cpk3x2)/norm(x(n+1:n+m)));
 fprintf('\n%s - iters = %d\n', cpk_string3, cpk3stats.niters);
 pause
 
-% restarted CPGMRES.
-fprintf('\n\n******************* %s(%d)*******************\n\n', cpk_string4, opts.restart)
+% CPMINRES.
+fprintf('\n\n********************* %s *********************\n\n', cpk_string4)
 [cpk4x,  cpk4stats,  cpk4flag] =  reg_cpkrylov(cpk4, b, Q, A, C, G, opts);
 cpk4x1  = cpk4x(1:n);
 cpk4x2  = cpk4x(n+1:n+m);
-fprintf('\n%s - rel err in (x, y) (refsol bsl): %7.1e', cpk_string4, norm(x-cpk4x)/norm(x));
-fprintf('\n%s - rel err in x (refsol bsl): %7.1e', cpk_string4, norm(x(1:n)-cpk4x1)/norm(x(1:n)));
-fprintf('\n%s - rel err in y (refsol bsl): %7.1e\n', cpk_string4, norm(x(n+1:n+m)-cpk4x2)/norm(x(n+1:n+m)));
+fprintf('\n%s - rel err in (x, y) (refsol bsl): %8.2e', cpk_string4, norm(x-cpk4x)/norm(x));
+fprintf('\n%s - rel err in x (refsol bsl): %8.2e', cpk_string4, norm(x(1:n)-cpk4x1)/norm(x(1:n)));
+fprintf('\n%s - rel err in y (refsol bsl): %8.2e\n', cpk_string4, norm(x(n+1:n+m)-cpk4x2)/norm(x(n+1:n+m)));
 fprintf('\n%s - iters = %d\n', cpk_string4, cpk4stats.niters);
+pause
+
+% CPSYMMLQ.
+fprintf('\n\n********************* %s *********************\n\n', cpk_string5)
+[cpk5x,  cpk5stats,  cpk5flag] =  reg_cpkrylov(cpk5, b, Q, A, C, G, opts);
+cpk5x1  = cpk5x(1:n);
+cpk5x2  = cpk5x(n+1:n+m);
+fprintf('\n%s - rel err in (x, y) (refsol bsl): %8.2e', cpk_string5, norm(x-cpk5x)/norm(x));
+fprintf('\n%s - rel err in x (refsol bsl): %8.2e', cpk_string5, norm(x(1:n)-cpk5x1)/norm(x(1:n)));
+fprintf('\n%s - rel err in y (refsol bsl): %8.2e\n', cpk_string5, norm(x(n+1:n+m)-cpk5x2)/norm(x(n+1:n+m)));
+fprintf('\n%s - iters = %d\n', cpk_string5, cpk5stats.niters);
+pause
+
+% CPSYMMLQ-V0.
+fprintf('\n\n********************* %s *********************\n\n', cpk_string6)
+[cpk6x,  cpk6stats,  cpk6flag] =  reg_cpkrylov(cpk6, b, Q, A, C, G, opts);
+cpk6x1  = cpk6x(1:n);
+cpk6x2  = cpk6x(n+1:n+m);
+fprintf('\n%s - rel err in (x, y) (refsol bsl): %8.2e', cpk_string6, norm(x-cpk6x)/norm(x));
+fprintf('\n%s - rel err in x (refsol bsl): %8.2e', cpk_string6, norm(x(1:n)-cpk6x1)/norm(x(1:n)));
+fprintf('\n%s - rel err in y (refsol bsl): %8.2e\n', cpk_string6, norm(x(n+1:n+m)-cpk6x2)/norm(x(n+1:n+m)));
+fprintf('\n%s - iters = %d\n', cpk_string6, cpk6stats.niters);
 pause
 
 figure;
       
-semilogy([0: size(cpk1stats.residHistory,1)-1],  cpk1stats.residHistory,  'm--x', ...
-         [0: size(cpk22stats.residHistory,1)-1], cpk22stats.residHistory, 'k:+', ...
-         [0: size(cpk2stats.residHistory,1)-1],  cpk2stats.residHistory,  'r-', ...
-         [0: size(cpk3stats.residHistory,1)-1],  cpk3stats.residHistory,  'b-.', ...
-         [0: size(cpk4stats.residHistory,1)-1],  cpk4stats.residHistory,  'g:', ...
+semilogy([0: size(cpk1stats.residHistory,1)-1],   cpk1stats.residHistory,   'b--+', ...
+         [0: size(cpk2stats.residHistory,1)-1],   cpk2stats.residHistory,   'r-.>', ...
+         [0: size(cpk3stats.residHistory,1)-1],   cpk3stats.residHistory,   'k-o', ...
+         [0: size(cpk4stats.residHistory,1)-1],   cpk4stats.residHistory,   'g-.s', ...
+         [0: size(cpk5stats.cgresidHistory,1)-1], cpk5stats.cgresidHistory, 'k:d', ...
+         [0: size(cpk5stats.qrresidHistory,1)-1], cpk5stats.qrresidHistory, 'r:x', ...
+         [0: size(cpk5stats.lqresidHistory,1)-1], cpk5stats.lqresidHistory, 'b-<', ...
+         [0: size(cpk6stats.cgresidHistory,1)-1], cpk6stats.cgresidHistory, 'c:+', ...
+         [0: size(cpk6stats.lqresidHistory,1)-1], cpk6stats.lqresidHistory, 'm:o', ...
          'LineWidth', 2);
 
-legend('CPCG', 'CPCGLANCZOS2', 'CPCGLANCZOS', 'CPDQGMRES', 'CPGMRES');
+legend('CPCG', 'CPCGLANCZOS','CPDQGMRES','CPMINRES', ...
+       'CPSYMMLQ (CG res)', 'CPSYMMLQ (QR res)','CPSYMMLQ (LQ res)', ...
+       'CPSYMMLQ-V0 (CG res)','CPSYMMLQ-V0 (LQ res)','Location','SouthWest');
 xlabel('iters');
 ylabel('log(residual)');
-title('Residual History');
+title('Residual history');
