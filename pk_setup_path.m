d883a60ea3baccc74197ebf1c43d1173e392d0aa
Dominique Orban <dominique.orban@gmail.com>
2019-10-18 12:16:04 -0400

    repository cleanup

--- pk_setup_path.m
+++
@@ -1,9 +0,0 @@
-subdirs = {'.', 'util', 'ops', 'kernels'};
-nSubdirs = size(subdirs, 2);
-for k = 1 : nSubdirs
-  p = sprintf('%s/%s', pwd, subdirs{k});
-  addpath(p);
-end
-clear p k nSubdirs subdirs;
-global pk_home_dir;
-pk_home_dir = pwd;
